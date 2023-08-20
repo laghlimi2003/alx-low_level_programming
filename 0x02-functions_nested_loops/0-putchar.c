08e0c7247927f55bc1819e4e9cae3a86134a9867
laghlimi2003 <laghlimieducation.01@gmail.com>
2023-08-20 12:56:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-/* 0-putchar.c */
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
 int main(void)
 {
     _putchar('_');
