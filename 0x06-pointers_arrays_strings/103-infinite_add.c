a83515894aee8d563d7e3e4ee9e60ea2f17c5c4c
laghlimi2003 <laghlimieducation.01@gmail.com>
2023-08-26 07:49:14 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,66 +1,87 @@
-#include <stdio.h>
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-int main(void)
-{
-    // Your test cases here
-    return 0;
-}
-
+/**
+ * infinite_add - adds two integers stored as strings
+ *
+ * @n1: first integer string to add
+ * @n2: second integer string to add
+ * @r: array to store resulting string in
+ * @size_r: size of array r
+ *
+ * Return: the summed string in r. If r is too small for the result,
+ * return 0;
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, carry = 0, sum, i, j;
-    
-    // Calculate the lengths of the input strings
-    while (n1[len1] != '\0')
-        len1++;
-    while (n2[len2] != '\0')
-        len2++;
-    
-    // Check if the result can fit in the buffer
-    if (len1 >= size_r || len2 >= size_r)
-        return 0;
-    
-    // Start adding from the least significant digit
-    i = len1 - 1;
-    j = len2 - 1;
-    
-    int result_index = 0;
-    
-    while (i >= 0 || j >= 0 || carry > 0)
-    {
-        // Calculate the sum of digits at the current positions
-        sum = carry;
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
-        
-        // Calculate the new carry and the current digit
-        carry = sum / 10;
-        sum = sum % 10;
-        
-        // Store the digit in the result buffer
-        r[result_index] = sum + '0';
-        
-        // Move to the next position
-        i--;
-        j--;
-        result_index++;
-    }
-    
-    // Null-terminate the result string
-    r[result_index] = '\0';
-    
-    // Reverse the result string
-    for (i = 0, j = result_index - 1; i < j; i++, j--)
-    {
-        char temp = r[i];
-        r[i] = r[j];
-        r[j] = temp;
-    }
-    
-    return r;
-}
+	int carry = 0, index = 0, index2;
+	char *s1 = n1, *s2 = n2;
 
+	while (*s1 != 0)
+		s1++;
+	while (*s2 != 0)
+		s2++;
+	size_r--;
+	r[size_r] = 0;
+	s1--;
+	s2--;
+	while (s2 != n2 - 1 && s1 != n1 - 1)
+	{
+		r[index] = *s2 - '0' + *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry++;
+			r[index] -= 10;
+		}
+		index++;
+		s2--;
+		s1--;
+		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
+			return (0);
+	}
+	while (s1 != n1 - 1)
+	{
+		r[index] = *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s1--;
+		index++;
+		if (size_r == index && (s1 != n1 - 1 ||  carry == 1))
+			return (0);
+	}
+	while (s2 != n2 - 1)
+	{
+		r[index] = *s2 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s2--;
+		index++;
+		if (size_r == index && (s2 != n2 - 1 || carry == 1))
+			return (0);
+	}
+	if (carry == 1)
+	{
+		r[index] = '1';
+		r[index + 1] = 0;
+	}
+	else
+	{
+		r[index--] = 0;
+	}
+	index2 = 0;
+	while (index2 <= index)
+	{
+		carry = r[index];
+		r[index] = r[index2];
+		r[index2] = carry;
+		index--;
+		index2++;
+	}
+	return (r);
+}
