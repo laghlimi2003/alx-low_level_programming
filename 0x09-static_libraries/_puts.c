bc07a43ced54da3873aa089aaf8d85700eca472f
laghlimi2003 <laghlimieducation.01@gmail.com>
2023-09-02 09:41:02 -0700

    libmy.a

---
+++ 0x09-static_libraries/_puts.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+void _puts(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        _putchar(s[i]);
+        i++;
+    }
+}
---
+++ 0x09-static_libraries/_strstr.c
@@ -0,0 +1,20 @@
+char *_strstr(char *haystack, char *needle)
+{
+    if (*needle == '\0')
+    {
+        return haystack;
+    }
+
+    while (*haystack != '\0')
+    {
+        char *h = haystack;
+        char *n = needle;
+
+        while (*n != '\0' && *h == *n)
+        {
+            h++;
+            n++;
+        }
+
+        if (*n == '\0')
+        {
