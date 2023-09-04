4dea2e551d0a98ac76e1188a70fdcb6f56992c1c
laghlimi2003 <laghlimieducation.01@gmail.com>
2023-09-04 13:36:57 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,43 +1,108 @@
 #include <stdlib.h>
-#include "main.h"
+#include <stdio.h>
 
-/**
-* **strtow - function that splits a string into words.
-* @str : pointer
-* Return: Char
-*/
+static int count_words(char *str) {
+    int count = 0;
+    int in_word = 0;
 
-char **strtow(char *str)
-{
-	char **d;
-	int i;
-	int j = 0;
-	int con = 0;
+    while (*str) {
+        if (*str == ' ' || *str == '\t' || *str == '\n') {
+            in_word = 0;
+        } else if (in_word == 0) {
+            in_word = 1;
+            count++;
+        }
+        str++;
+    }
 
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
-			con++;
-	}
-
-	d = malloc(sizeof(char) * con);
-
-	if (d == NULL)
-		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
-		{
-			*d[j] = str[i];
-			j++;
-		}
-		else
-		{
-		}
-	}
-	return (d);
+    return count;
 }
+
+static char *strdup_word(char *start, char *end) {
+    int len = end - start;
+    char *word = (char *)malloc(len + 1);
+    
+    if (word == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < len; i++) {
+        word[i] = start[i];
+    }
+
+    word[len] = '\0';
+    return word;
+}
+
+char **strtow(char *str) {
+    if (str == NULL || *str == '\0') {
+        return NULL;
+    }
+
+    int num_words = count_words(str);
+    char **word_array = (char **)malloc((num_words + 1) * sizeof(char *));
+
+    if (word_array == NULL) {
+        return NULL;
+    }
+
+    int in_word = 0;
+    char *word_start = NULL;
+    int word_index = 0;
+
+    while (*str) {
+        if (*str == ' ' || *str == '\t' || *str == '\n') {
+            if (in_word) {
+                word_array[word_index] = strdup_word(word_start, str);
+                if (word_array[word_index] == NULL) {
+                    // Memory allocation failed
+                    for (int i = 0; i < word_index; i++) {
+                        free(word_array[i]);
+                    }
+                    free(word_array);
+                    return NULL;
+                }
+                word_index++;
+                in_word = 0;
+            }
+        } else if (in_word == 0) {
+            in_word = 1;
+            word_start = str;
+        }
+        str++;
+    }
+
+    if (in_word) {
+        word_array[word_index] = strdup_word(word_start, str);
+        if (word_array[word_index] == NULL) {
+            // Memory allocation failed
+            for (int i = 0; i <= word_index; i++) {
+                free(word_array[i]);
+            }
+            free(word_array);
+            return NULL;
+        }
+        word_index++;
+    }
+
+    word_array[word_index] = NULL;
+    return word_array;
+}
+
+int main(void) {
+    char **tab;
+
+    tab = strtow("      ALX School         #cisfun      ");
+    if (tab == NULL) {
+        printf("Failed\n");
+        return 1;
+    }
+
+    for (int i = 0; tab[i] != NULL; i++) {
+        printf("%s$\n", tab[i]);
+        free(tab[i]); // Free the allocated words
+    }
+
+    free(tab); // Free the array of pointers
+    return 0;
+
